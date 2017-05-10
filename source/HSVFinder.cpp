18c2a7692f414289eb26da2c834c61ef1f3e3504
Chris Chan <cchan9528@gmail.com>
2017-05-09 21:00:54 -0700

    Determined optimal HSVs for exposure. Better LEGO detector made.

--- source/HSVFinder.cpp
+++ source/HSVFinder.cpp
@@ -6,44 +6,29 @@
 //              H=[0,1,...,179]
 //              S=[0,1,...,255]
 //              V=[0,1,...,255]
+//
+//      Note: Color Inherently Depends on Exposure
+//            Thus need to determine exposure for picture
+//              or at least equalize histogram
+//
+
 
 #include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
-/* #define RED_H_LO 0
-#define RED_S_LO 76
-#define RED_V_LO 129
-#define RED_H_HI 6
-#define RED_S_HI 255
-#define RED_V_HI 255
-#define BLUE_H_LO 73
-#define BLUE_S_LO 42
-#define BLUE_V_LO 122
-#define BLUE_H_HI 126
-#define BLUE_S_HI 168
-#define BLUE_V_HI 214
-#define GREEN_H_LO 54
-#define GREEN_S_LO 25
-#define GREEN_V_LO 0
-#define GREEN_H_HI 80
-#define GREEN_S_HI 211
-#define GREEN_V_HI 197
-#define WHITE_H_LO 11
-#define WHITE_S_LO 0
-#define WHITE_V_LO 221
-#define WHITE_H_HI 23
-#define WHITE_S_HI 33
-#define WHITE_V_HI 255
-#define YELLOW_H_LO 20
-#define YELLOW_S_LO 115
-#define YELLOW_V_LO 204
-#define YELLOW_H_HI 28
-#define YELLOW_S_HI 255
-#define YELLOW_V_HI 255 */
-
 using namespace std;
+
+int lowH = 0, highH = 0;
+int lowS = 0, highS = 0;
+int lowV = 0, highV = 0;
+
+void onChange(int, void*)
+{
+    cout<<"low HSV: " << lowH << ", " << lowS <<", "<< lowV <<endl;
+    cout<<"high HSV: " << highH << ", " << highS <<", "<< highV <<endl;
+}
 
 int main(int argc, char** argv)
 {
@@ -55,16 +40,13 @@ int main(int argc, char** argv)
     }
 
     // Set up Slider GUI
-    int lowH = 0, highH = 0;
-    int lowS = 0, highS = 0;
-    int lowV = 0, highV = 0;
     cv::namedWindow("HSV Control", CV_WINDOW_NORMAL);
-    cv::createTrackbar("[H]ue Lo", "HSV Control", &lowH, 179);
-    cv::createTrackbar("[H]ue Hi", "HSV Control", &highH, 179);
-    cv::createTrackbar("[S]at Lo", "HSV Control", &lowS, 255);
-    cv::createTrackbar("[S]at Hi", "HSV Control", &highS, 255);
-    cv::createTrackbar("[V]al Lo", "HSV Control", &lowV, 255);
-    cv::createTrackbar("[V]al Hi", "HSV Control", &highV, 255);
+    cv::createTrackbar("[H]ue Lo", "HSV Control", &lowH, 179, onChange);
+    cv::createTrackbar("[H]ue Hi", "HSV Control", &highH, 179, onChange);
+    cv::createTrackbar("[S]at Lo", "HSV Control", &lowS, 255, onChange);
+    cv::createTrackbar("[S]at Hi", "HSV Control", &highS, 255, onChange);
+    cv::createTrackbar("[V]al Lo", "HSV Control", &lowV, 255, onChange);
+    cv::createTrackbar("[V]al Hi", "HSV Control", &highV, 255, onChange);
 
     // Capture Frame from Source
     cv::Mat frame = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);   // Note: BGR on Load
@@ -74,7 +56,9 @@ int main(int argc, char** argv)
         cout << "HSVFinder Terminated on Read Image Error." << endl;
         return -1;
     }
-    cv::imshow("Original Image", frame);
+    cv::Mat resized;
+    cv::resize(frame, resized, cv::Size(), .15, .15);
+    cv::imshow("Original Image", resized);
 
     // HSV Finder
     cout<<"Processing...\n\n"<<endl;
@@ -83,18 +67,15 @@ int main(int argc, char** argv)
         // Convert Frame to HSV
         cv::Mat frameInHSV;
         cvtColor(frame, frameInHSV, cv::COLOR_BGR2HSV);
-        cout<<"Done converting to HSV."<<endl;
 
         // Threshold Frame According to What User Adjusts HSV Intervals to
-        cout<<"low HSV: " << lowH << ", " << lowS <<", "<< lowV <<endl;
-        cout<<"high HSV: " << highH << ", " << highS <<", "<< highV <<endl;
         cv::Mat mask;
         cv::Scalar lowerBound(lowH, lowS, lowV), upperBound(highH, highS, highV);
         cv::inRange(frameInHSV, lowerBound, upperBound, mask);
-        cout<<"Done Thresholding."<<endl;
 
         // Display the (Threshold) Mask
-        cv::imshow("Thresholded Mask", mask);
+        cv::resize(mask, resized, cv::Size(), .15, .15);
+        cv::imshow("Thresholded Mask", resized);
         if(cv::waitKey(30) == 27)
         {
             cout<<"\n\n\nUser held esc key to terminate program"<<endl;
@@ -104,5 +85,6 @@ int main(int argc, char** argv)
 
     // Exit
     cout<< "\n\n\nHSVFinder Terminated on Success." << endl;
+
     return 0;
 }
--- source/LEGOFinder.cpp
+++ source/LEGOFinder.cpp
@@ -4,6 +4,8 @@
 #include <opencv2/imgproc.hpp>
 #include "legohsvcolors.hpp"
 
+#include AREA_THRESHOLD 5000        // Inherent Scale Variance
+
 using namespace std;
 
 int main(int argc, char** argv)
@@ -24,15 +26,14 @@ int main(int argc, char** argv)
         return -1;
     }
     cv::Mat frameInHSV;
-    cvtColor(frame, frameInHSV, cv::COLOR_BGR2HSV);
-    cout<<"Done converting to HSV."<<endl;
+    cv::cvtColor(frame, frameInHSV, cv::COLOR_BGR2HSV);
 
     // Define HSV LEGO Color Bounds
     cv::Scalar greenLB(GREEN_H_LO, GREEN_S_LO, GREEN_V_LO);
     cv::Scalar greenUB(GREEN_H_HI, GREEN_S_HI, GREEN_V_HI);
     cv::Scalar blueLB(BLUE_H_LO, BLUE_S_LO, BLUE_V_LO);
     cv::Scalar blueUB(BLUE_H_HI, BLUE_S_HI, BLUE_V_HI);
-    cv::Scalar redLB(GREEN_H_LO, RED_S_LO, RED_V_LO);
+    cv::Scalar redLB(RED_H_LO, RED_S_LO, RED_V_LO);
     cv::Scalar redUB(RED_H_HI, RED_S_HI, RED_V_HI);
     cv::Scalar yellowLB(YELLOW_H_LO, YELLOW_S_LO, YELLOW_V_LO);
     cv::Scalar yellowUB(YELLOW_H_HI, YELLOW_S_HI, YELLOW_V_HI);
@@ -50,32 +51,51 @@ int main(int argc, char** argv)
     cv::inRange(frameInHSV, yellowLB, yellowUB, yellowMask);
     cv::Mat whiteMask;
     cv::inRange(frameInHSV, whiteLB, whiteUB, whiteMask);
-    cv::Mat cumMask = greenMask;
-    cv::Mat masks[] = {greenMask, blueMask, redMask, yellowMask, whiteMask};
-    for(int i = 0 ; i < 5; i++)
-        cv::bitwise_or(cumMask, masks[i], cumMask);
 
     // Display the Masks
-    string windowName[7] = {"Original", "Green Mask", "Blue Mask","Red Mask",
-                            "Yellow Mask", "White Mask", "Cumulative Mask"};
-    cv::Mat image[7] = {frame, greenMask, blueMask, redMask,
-                        yellowMask, whiteMask, cumMask};
-    for(int i = 0; i < 7; i++)
+    string windowName[5] = {"Green Mask", "Blue Mask","Red Mask",
+                            "Yellow Mask", "White Mask"};
+    cv::Mat image[5] = {greenMask, blueMask, redMask, yellowMask, whiteMask};
+    cv::Mat cumMask;
+    for(int i = 0; i < 5; i++)
     {
+        // LPF for Noise
+        cv::blur(image[i], image[i], cv::Size(5,5));
+
+        // Find Contours Around Blobs
+        vector< vector<cv::Point> > contours;
+        cv::findContours(image[i].clone(), contours, // hierarchy,
+                        CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+        // Draw Bounding Boxes Around LEGOs (Determined by Area)
+        cv::Mat imageInBGR;
+        cv::cvtColor(image[i], imageInBGR, CV_GRAY2BGR);
+        for(int i = 0; i < contours.size(); i++)
+        {
+            if(cv::contourArea(contours[i]) < AREA_THRESHOLD)
+                continue;
+            cv::Rect r = cv::boundingRect(contours[i]);
+            cv::rectangle(imageInBGR, r, cv::Scalar(0,255,0), 8);
+        }
+
+        // DEBUG
         cv::Mat resized;
-        cv::resize(image[i], resized, cv::Size(), .25, .25);
-        cv::imshow(windowName[i], resized);
+        cv::resize(imageInBGR, resized, cv::Size(), .25, .25);
+        cv::imshow(windowName[i] + "(blurred)", resized);
+
+        // Add Found Pieces to Cumulative Mask
+        cumMask = (i==0) ? imageInBGR : cumMask + imageInBGR;
     }
 
-    // For Observations
+    // Show Cumulative Mask
+    cv::Mat resized;
+    cv::resize(cumMask, resized, cv::Size(), .25, .25);
+    cv::imshow("Cumulative Mask", resized);
+
+    // For Observation
     while(true)
-    {
         if(cv::waitKey(30) == 27)
-        {
-            cout<<"\n\n\nUser held esc key to terminate program"<<endl;
-            break;
-        }
-    }
+            cout<<"\n\n\nUser held esc key to terminate program"<<endl; break;
 
     // Exit
     cout<< "\n\n\nHSVFinder Terminated on Success." << endl;
